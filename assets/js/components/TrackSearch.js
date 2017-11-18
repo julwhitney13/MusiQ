d6b828842c0eebe7b8fd2547b3cf4b05025fb501
Kim <whitney.k@husky.neu.edu>
2017-11-18 17:28:03 -0500

    form

--- assets/js/components/TrackSearch.js
+++ assets/js/components/TrackSearch.js
@@ -1,39 +1,66 @@
 import React, {Component } from 'react';
 import { form, FormGroup, FormControl, InputGroup, Glyphicon, ControlLabel, Button} from 'react-bootstrap';
+import axios from 'axios';
 
 export default class TrackSearch extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+          songsearch: false,
+          songquery: '',
+          artistsearch: false,
+          artistquery: '',
+          albumsearch: false,
+          albumquery: '',
+        }
+    }
+    submitForm(e) {
+        e.preventDefault()
+        var params = []
+        if (songsearch) {
+            params.push({"q": this.state.songquery, "type": "track"})
+        }
+        if (artistsearch) {
+            params.push({"q": this.state.artistquery, "type": "artist"})
+        }
+        if (albumquery) {
+            params.push({"q": this.state.albumquery, "type": "album"})
+        }
+
+        axios.get('/search', params)
+            .then(res => { console.log(JSON.stringify(res))
+        });
+
     }
 
     render() {
         return (
-            <form>
-                <ControlLabel>Song</ControlLabel>
+            <form onSubmit={this.submitForm}>
+                <ControlLabel>Search by song</ControlLabel>
                 <FormGroup>
                   <InputGroup>
                     <InputGroup.Addon>
-                      <input type="checkbox" aria-label="songsearch" />
+                      <input type="checkbox" name="songsearch" checked={this.state.songsearch} />
                     </InputGroup.Addon>
-                    <FormControl type="text" label="track" placeholder="Enter a song name..."/>
+                    <FormControl type="text" name="songquery" value={this.state.songquery}  placeholder="Enter a song name..."/>
                   </InputGroup>
                 </FormGroup>
-                <ControlLabel>Artist</ControlLabel>
+                <ControlLabel>Search by artist</ControlLabel>
                 <FormGroup>
                   <InputGroup>
                     <InputGroup.Addon>
-                      <input type="checkbox" aria-label="artistsearch" />
+                      <input type="checkbox" name="artistsearch" checked={this.state.artistsearch} />
                     </InputGroup.Addon>
-                    <FormControl type="text" label="artist" placeholder="Enter an artist..."/>
+                    <FormControl type="text" name="artistquery" value={this.state.artistquery} placeholder="Enter an artist..."/>
                   </InputGroup>
                 </FormGroup>
-                <ControlLabel>Album</ControlLabel>
+                <ControlLabel>Search by album</ControlLabel>
                 <FormGroup>
                   <InputGroup>
                     <InputGroup.Addon>
-                      <input type="checkbox" aria-label="songsearch" />
+                      <input type="checkbox" name="albumsearch" checked={this.state.albumsearch} />
                     </InputGroup.Addon>
-                    <FormControl type="text" label="album" placeholder="Enter an album..."/>
+                    <FormControl type="text" name="albumquery" value={this.state.albumquery}  placeholder="Enter an album..."/>
                   </InputGroup>
                 </FormGroup>
                 <Button type="submit">
