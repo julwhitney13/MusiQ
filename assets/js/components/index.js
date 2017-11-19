cb7b6140cbb2bea2d268c158b97623dc2ae0ce52
Kim <whitney.k@husky.neu.edu>
2017-11-19 13:15:29 -0500

    wip

--- assets/js/components/index.js
+++ assets/js/components/index.js
@@ -1,6 +1,7 @@
 // Base code from react-dnd example: http://react-dnd.github.io/react-dnd/examples-sortable-simple.html
 import React, { Component } from 'react'
 import Container from './Container'
+import TrackSearch from './TrackSearch'
 
 export default class SortableSimple extends Component {
     constructor(props) {
@@ -11,10 +12,17 @@ export default class SortableSimple extends Component {
     render() {
         return (
             <div>
-                <h1>
-                    Up Next:
-                </h1>
-                <Container />
+                <row>
+                    <div style="col-md-8">
+                        <h1>
+                            Our Queue:
+                        </h1>
+                        <Container />
+                    </div>
+                    <div style="col-md-4">
+                        <TrackSearch />
+                    </div>
+                </row>
             </div>
         )
     }
